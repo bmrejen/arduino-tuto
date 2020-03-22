1313c2b03f37eacbd33e73237d262be9ed8c4830
Benoit Mrejen <benoit.mrejen@gmail.com>
2020-03-22 16:16:18 +0100

    add voltage read

---
+++ sketch_mar18a/01_read_voltage.ino
@@ -0,0 +1,19 @@
+// read voltage from the circuit
+
+int readPin = A1;
+int readVal;
+float V2=0;
+int delayTime = 500;
+
+void setup() {
+  pinMode(readPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  readVal = analogRead(readPin);
+  V2 = 5./1023.*readVal;
+  Serial.println(V2);
+  delay(delayTime);
+
+}
